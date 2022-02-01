7cf9b30da1b32b569163919185d64cdee9c386f7
Mishel <rp18027@ues.edu.sv>
2022-01-31 21:53:53 -0600

    agregando rutas de recursos
    Esta es la manera manual, de igual forma existe la forma en la cual solo se pone una linea

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -11,8 +11,39 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
+
+// RUTAS DE RECURSOS, es importante el orden de las rutas ya que podrian ser enclipsadas
 
 Route::get('/', function () {
-    return view('welcome');
-});
+	return view('welcome');
+})->name('main');
+
+Route::get('products', function () {
+	return 'Esta es la lista de productos';
+})->name('products.index');
+
+//Muestra el formulario para crear un producto
+Route::get('products/create', function () {
+	return 'Formulario para crear productos';
+})->name('products.create');
+
+Route::post('products', function () {
+	//return 'Formulario para crear productos';
+})->name('products.store');
+
+Route::get('products/{product}', function ($product) {
+	return "Showing product with id {$product}";
+})->name('products.show');
+
+Route::get('products/{product}/edit', function ($product) {
+	return "Showing the form to edit the product with id {$product}";
+})->name('products.edit');
+
+Route::match(['put', 'patch'], 'products/{product}', function ($product) {
+	//return "Showing product with id {$product}";
+})->name('products.update');
+
+Route::delete('products/{product}', function ($product) {
+	//return "Showing product with id {$product}";
+})->name('products.destroy');
